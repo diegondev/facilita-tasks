37dcd827448df6ae317f948b9d7785e31bd0d605
Diego Nascimento <diegohfn@gmail.com>
2021-09-10 13:30:42 -0400

    Setup project.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,19 +40,19 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
+<style lang="stylus" scoped>
+h3
   margin: 40px 0 0;
-}
-ul {
+
+ul
   list-style-type: none;
   padding: 0;
-}
-li {
+
+li
   display: inline-block;
   margin: 0 10px;
-}
-a {
+
+a 
   color: #42b983;
-}
+
 </style>
