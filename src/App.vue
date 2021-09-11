db92aa39d2e219f410eee4101ad30ea22afbfe74
Diego Nascimento <diegohfn@gmail.com>
2021-09-10 23:07:18 -0400

    Update App component to clean unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,4 @@
 </template>
 
 <style lang="stylus">
-* 
-  margin: 0
-  padding: 0
-  box-sizing: border-box
-
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
 </style>
