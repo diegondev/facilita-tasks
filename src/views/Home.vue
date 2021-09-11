423bd31de57452e091a161451f55a237af6c98ea
Diego Nascimento <diegohfn@gmail.com>
2021-09-11 02:04:06 -0400

    Create structure for side list on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,102 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <aside class="categories-list-container">
+            <h3>Categorias</h3>
+            <ul class="category-list">
+                <li 
+                    v-for="(category, key) of categories" 
+                    :key="key" 
+                    class="category-item">
+                    {{ category.name }}
+                    <div v-if="category.quantity" 
+                        class="badge"
+                        :class="category.urgency">
+                        {{ category.quantity }}
+                    </div>
+                </li>
+            </ul>
+        </aside>
+        <main></main>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+    name: 'Home',
+    data() {
+        return {
+            categories: [
+                {
+                    name: 'Todas',
+                },
+                {
+                    name: 'Urgentes',
+                    quantity: 1,
+                    urgency: 'urgent'
+                },
+                {
+                    name: 'Importantes',
+                    quantity: 2,
+                    urgency: 'important'
+                },
+                {
+                    name: 'Outras',
+                },
+                {
+                    name: 'Finalizadas',
+                }
+            ]
+        }
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+.home
+  display flex
+  flex row
+  height 100%
+
+  aside
+    display flex
+    flex-direction column
+    align-items center
+    justify-content center
+    width 260px
+
+    h3
+        margin-left -24px
+
+  main
+    flex 1
+
+.category-list
+    font-size 16px
+    font-weight bold
+    margin 58px 0
+    list-style none
+
+    .category-item
+        margin-bottom 18px
+
+        &:before
+            content ''
+            display inline-block
+            margin-right 8px
+            height 12px
+            width 12px
+            background-image url('../assets/icons/chevron-right-solid.svg')
+            background-repeat no-repeat
+            background-size contain
+            background-position center
+
+        .badge
+            display inline-block
+            width 20px
+            height 20px
+            font-size 10px
+            padding 4px
+            text-align center
+            border-radius 50%
+</style>
