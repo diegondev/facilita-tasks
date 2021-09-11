ac1fba299cb47ff10a1d9494a12d4eba1e60fa85
Diego Nascimento <diegohfn@gmail.com>
2021-09-10 23:05:19 -0400

    Update login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,14 +5,30 @@
         </aside>
         <main>
             <form action="#">
-                <input type="text">
+                <h3 class="form-title">Entre com seus dados de accesso.</h3>
+
+                <label for="username">Nome de usuário ou e-mail</label>
+                <input id="username" name="username" type="text">
+
+                <label for="password">Senha</label>
+                <input id="password" type="password">
+
+                <button class="bg-primary" @click="login">Entrar</button>
+                <router-link to="/forget-password">Esqueci minha senha</router-link>
             </form>
         </main>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    methods: {
+        login: function () {
+            // Login validation
+            this.$router.push('/home')
+        }
+    }
+}
 </script>
 
 <style lang="stylus" scoped>
@@ -32,5 +48,37 @@ export default {}
     main
         flex 3
         display flex
+        flex-direction column
+        justify-content center
+        align-items: center
+
+        form 
+            display flex
+            flex-direction column
+            justify-content center
+            width 35%
+
+        .form-title
+            margin-bottom 42px
+            font-weight bold
+
+        label
+            font-size 12px
+            margin 8px 0
+
+        input
+            height 52px
+            border-radius 5px
+            padding 0 8px
+
+        button
+            height 52px
+            border-radius 5px
+            margin-top 18px
+            margin-bottom 42px
+
+    a
+        width 100%
+        font-size 12px
 
 </style>
