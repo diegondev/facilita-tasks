1b3b63b361f623bd640c1a6aa59aeef1d0a86db1
Diego Nascimento <diegohfn@gmail.com>
2021-09-10 20:14:04 -0400

    Organize files

---
+++ src/layouts/main/Main.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="main-container">
+        <Sidenav class="sidenav" />
+        <header class="header">...</header>
+        <div class="content">
+            <router-view />
+        </div>
+    </div>
+</template>
+
+<script>
+import Sidenav from './components/Sidenav.vue';
+export default {
+    components: {
+        Sidenav
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.main-container 
+    display grid
+    width 100%
+    height 100vh
+    grid-template-columns 106px 1fr
+    grid-template-rows 84px 1fr
+    grid-template-areas "sidenav header"
+                        "sidenav content";
+
+.sidenav
+    grid-column  1 / 2
+    grid-row 1 / 3
+
+.header
+    grid-column 2 /3
+    grid-row 1 / 2
+    background-color red
+
+.content
+    grid-column 2
+    grid-row 2
+    grid-area 'content'
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Main from '../layouts/Main.vue';
+import Main from '../layouts/main/Main.vue';
 import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 
